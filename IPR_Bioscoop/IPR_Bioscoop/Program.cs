e457e79c690f0e361d1f74844ec31320c73b2388
Bipin <floriantjager@gmail.com>
2020-10-06 11:06:30 +0200

    Create project

---
+++ IPR_Bioscoop/IPR_Bioscoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IPR_Bioscoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
