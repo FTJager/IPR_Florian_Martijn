13654051fad004340ef5c6869e62bfc199772610
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-10-07 11:43:22 +0200

    Made a class for Films

---
+++ IPR_Bioscoop/Film/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Film
+{
+    class Film
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public int Length { get; set; }
+        public string Description { get; set; }
+        public int review { get; set; }
+        public int TicketsLeft { get; set; }
+
+        public Film(string Title, DateTime date, int Length, string Description, int review, int Tickets)
+        {
+            this.Title = Title;
+            this.Date = date;
+            this.Length = Length;
+            this.Description = Description;
+            this.review = review;
+            this.TicketsLeft = Tickets;
+        }
+
+    }
+}
