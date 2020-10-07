e4cc570a1e266054579d20c36e30f0cb5b0d994a
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-10-07 11:36:22 +0200

    Made some more small GUI changes

--- IPR_Bioscoop/GUI/ViewModels/MainViewModel.cs
+++ IPR_Bioscoop/GUI/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
-﻿using GUI.Utils;
+﻿using GalaSoft.MvvmLight.Command;
+using GUI.Utils;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace GUI.ViewModels
 {
@@ -15,5 +17,26 @@ namespace GUI.ViewModels
             ViewModel = new ItemListViewModel(this);
         }
 
+
+        private ICommand mSearchCommand;
+        public ICommand SearchCommand
+        {
+            get
+            {
+                if(mSearchCommand == null)
+                {
+                    mSearchCommand = new RelayCommand(() =>
+                    {
+                        Search("WIP");
+                    });
+                }
+                return mSearchCommand;
+            }
+        }
+
+        private void Search (string Title)
+        {
+            Console.WriteLine(Title);
+        }
     }
 }
