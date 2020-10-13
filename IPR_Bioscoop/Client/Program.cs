a0c023d206fbeb6f60072d54b46cb95b0115c652
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-10-13 10:06:30 +0200

    Made basic client

--- IPR_Bioscoop/Client/Program.cs
+++ IPR_Bioscoop/Client/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
 
 namespace Client
 {
     class Program
     {
+
+        private static TcpClient client;
+        private static NetworkStream stream;
+        private static string username;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Client");
+            Console.WriteLine("What is your username");
+            username = Console.ReadLine();
+
+            client = new TcpClient();
+            client.BeginConnect("localhost", 84573, new AsyncCallback(OnConnect), null);
+        }
+
+        private static void OnConnect(IAsyncResult ar)
+        {
+            client.EndConnect(ar);
+            Console.WriteLine("Verbonden");
         }
     }
 }
