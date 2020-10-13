7702356afceb5ed52c591903f6cbf1891be9efc2
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-10-13 12:59:02 +0200

    Update the GUI

---
+++ IPR_Bioscoop/GUI/Utils/ObservableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GUI.Utils
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ IPR_Bioscoop/GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight.Command;
+using GUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GUI.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public ICommand searchTitle { get; set; }
+
+        public string filmTitle { get; set; }
+
+        public MainViewModel ()
+        {
+            searchTitle = new RelayCommand(() =>
+           {
+               filmTitle = "het werkt!!";
+           });
+        }
+
+    }
+}
