f940f96c3a501de399e15ae2e157028c72eff394
Bipin <floriantjager@gmail.com>
2020-10-23 22:01:54 +0200

    e

--- IPR_Bioscoop/Server/ClientHandling.cs
+++ IPR_Bioscoop/Server/ClientHandling.cs
@@ -37,7 +37,6 @@ namespace Server
             string id = "";
             JsonElement jsonCommand = JsonDocument.Parse(packetData).RootElement;   //Converts packetData back into a JSON string
             id = jsonCommand.GetProperty("id").GetString().Substring(0, jsonCommand.GetProperty("id").GetString().IndexOf("/"));
-            Console.WriteLine(id);
 
             switch (id)
             {
@@ -123,7 +122,6 @@ namespace Server
         internal void MoviesCommandHandling(JsonElement command)
         {
             string id = command.GetProperty("id").GetString().Substring(command.GetProperty("id").GetString().IndexOf("/") + 1);    //Only gets the second part of the id
-            Console.WriteLine(id);
 
             switch (id)
             {
